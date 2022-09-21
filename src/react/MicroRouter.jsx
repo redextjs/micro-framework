98e4f8dea5ab97dd88108bcfb08efb81ef8f3b50
donglh <huudong.ptit@gmail.com>
2022-09-21 17:18:24 +0700

    update MicroRouter.jsx

--- src/react/MicroRouter.jsx
+++ src/react/MicroRouter.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter, StaticRouter } from 'react-router-dom';
 
 import getMicroState from '../getMicroState';
 
 export const MicroRouter = ({ appName, children }) => {
   const microState = getMicroState(appName);
 
-  const { isHash, activePath: basename = '/' } = microState;
+  const { isHash, activePath: basename = '/', staticPath } = microState;
 
-  const Router = isHash ? HashRouter : BrowserRouter;
+  const routerProps = {
+    basename
+  };
+
+  let Router;
+
+  if (staticPath) {
+    Router = StaticRouter;
+
+    routerProps.location = {
+      pathname: staticPath
+    }
+  } else {
+    Router = isHash ? HashRouter : BrowserRouter;
+  }
 
   return (
-    <Router basename={basename}>
+    <Router {...routerProps}>
       {children}
     </Router>
   )
